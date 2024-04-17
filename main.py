bf59c830c87947bf50acc2b1748aaadd4ff33724
Bailey Nield <bailey.nield18@gmail.com>
2024-04-17 09:40:32 -0600

    Add CRUD operations for FastAPI

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import FastAPI
 
 from models import Item
@@ -5,7 +7,33 @@ from models import Item
 
 app = FastAPI()
 
+with open("store_items.json", "r") as f:
+    data = json.load(f)
 
-@app.get("item")
-async def list_item() -> list[Item]:
-    
+items: list[Item] = []
+
+for item in data:
+    items.append(Item(**item))
+
+
+@app.get("/items")
+async def list_items() -> list[Item]:
+    return items
+
+@app.post("/items")
+async def creat_items(item: Item) -> None:
+    items.append(item)
+
+@app.put("/items/{item_id}")
+async def update_items(item_id: int, updated_item: Item) -> None:
+    for x, item in enumerate(items):
+        if item.id == item_id:
+            items[x] = updated_item
+            return
+
+@app.delete("/items/{item_id}")
+async def delete_items(item_id: int) -> None:
+    for x, item in enumerate(items):
+        if item.id == item_id:
+            items.pop(x)
+            return
